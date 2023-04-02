d391062f37ea8db71c373a491d71b4974e5e2225
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2023-04-02 18:06:27 +0600

    drawer navbar, tailwindCSS, ReactRouter basic setup

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DrawerNav from "../Components/Isolated/DrawerNav";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <DrawerNav />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
